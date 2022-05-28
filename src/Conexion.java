f071b09838c4e324cb14e81524bbf5ffb481d81d
DenilsonAla19 <dalasama9@gmail.com>
2022-05-28 16:30:00 -0500

    Se creó y comprobó la conexión con la base de datos MySQL y se agregó una nueva librería al proyecto.

--- src/Conexion.java
+++ src/Conexion.java
@@ -3,19 +3,37 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 public class Conexion {
-	public static void main(String[] args) {
+    Connection con;
+	
+    public Conexion() {
         try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://bcoezkam4s8dlb2yyc7o-mysql.services.clever-cloud.com:3306/bcoezkam4s8dlb2yyc7o/bcoezkam4s8dlb2yyc7o","ujib6gic4laepk37","qPz2XYXURrrWmEm6XPCH");
-            Statement stmt=con.createStatement();  
-            ResultSet rs=stmt.executeQuery("show databases;");
+        { 
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://ujib6gic4laepk37:qPz2XYXURrrWmEm6XPCH@bcoezkam4s8dlb2yyc7o-mysql.services.clever-cloud.com:3306/bcoezkam4s8dlb2yyc7o",
+                    "ujib6gic4laepk37",
+                    "qPz2XYXURrrWmEm6XPCH");
+
             System.out.println("Connected");  
         }
         catch(Exception e)
         {
             System.out.println(e);
         }
-    }  
+    }
+    public static void main(String[] args) {
+        Conexion cn = new Conexion();
+        Statement st;
+        ResultSet rs;
+        try {
+            st = cn.con.createStatement();
+            rs = st.executeQuery("select * from producto");
+            while(rs.next()) {
+                System.out.println(rs.getString("idProducto")+" "+rs.getString("nameProducto")+" "+rs.getString("fechaIngreso")+" "+rs.getString("idProveedor"));
+            }
+            cn.con.close();
+        } catch (Exception e){
+            System.out.println("Error");
+        }
+                
+    }
 }
